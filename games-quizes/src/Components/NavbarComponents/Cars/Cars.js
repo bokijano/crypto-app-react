33973fb2f46101c99355c4e7522fc50dd99e0532
Bojan Krstic <krstic.bojan80@gmail.com>
2019-03-04 23:01:55 +0100

    first commit

---
+++ games-quizes/src/Components/NavbarComponents/Cars/Cars.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cars = () => {
+  return (
+    <div>
+      <p style={{ marginTop: "100px" }}>This is a cars component</p>
+    </div>
+  );
+};
+
+export default Cars;
